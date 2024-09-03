33e81876642073181128520b2634e2c74a8adc27
TahirHameed74 <tsb_125@yahoo.com>
2024-09-04 00:36:37 +0500

    next js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  images: {
-    domains: [
-      "firebasestorage.googleapis.com",
-      "raw.githubusercontent.com",
-      "www.notion.so",
-    ],
-  },
+	images: {
+		domains: [
+			"firebasestorage.googleapis.com",
+			"raw.githubusercontent.com",
+			"www.notion.so",
+		],
+	},
+	assetPrefix: isProd ? "/your-repo-name/" : "",
 };
 
 module.exports = nextConfig;
