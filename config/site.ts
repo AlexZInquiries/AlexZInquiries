eb5d341cb90adfd11b50bac1a9c9e48382beea3a
TahirHameed74 <tsb_125@yahoo.com>
2024-09-04 01:00:32 +0500

    m

--- config/site.ts
+++ config/site.ts
@@ -1,11 +1,11 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Zhiyu Alex Zhang | 张智瑜 ",
-  description: "Zhiyu Alex Zhang's Personal Website",
-  links: {
-    github: "https://github.com/RealAlexZ",
-    linkedin: "https://www.linkedin.com", // To be fixed
-    email: "mailto:alexzinquiries@gmail.com",
-  },
+	name: "Zhiyu Alex Zhang | 张智瑜 ",
+	description: "Zhiyu Alex Zhang's Personal Website",
+	links: {
+		github: "https://github.com/RealAlexZ",
+		linkedin: "https://www.linkedin.com", // To be fixed
+		email: "mailto:alexzinquiries@gmail.com", // test
+	},
 };
