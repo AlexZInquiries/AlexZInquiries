df4507b2672d6ace42eb4b122ce5e7c18abd8369
AlexZInquiries <alexqinquiries@gmail.com>
2024-09-03 17:43:42 -0400

    Updated professional experience

--- components/professional-experience.tsx
+++ components/professional-experience.tsx
@@ -19,9 +19,19 @@ const ProfessionalExperience = () => {
                         <strong>R&D Intern</strong> - MicroPort NaviBot (May 2022 - Aug 2022)
                     </li>
                     <li>
-                        <strong>Freelance Translator</strong> (Jun 2018 - Present)
+                        <strong>Student Researcher</strong> - Center for Assessment of Elementary Education at the Ministry of Education (Sep 2020 - Aug 2024)
+                    </li>
+                    <li>
+                        <strong>Composer and Lyricist</strong>
                         <div className="mt-1">
-                            <p className="text-sm text-gray-600 dark:text-gray-400">• Shanghai Education Press House (Apr 2023 - Present)</p>
+                            <p className="text-sm text-gray-600 dark:text-gray-400">• Shanghai Music Publishing House (Sep 2022 - May 2024)</p>
+                            <p className="text-sm text-gray-600 dark:text-gray-400">• Zhejiang Education Publishing House (Sep 2020 - Aug 2024)</p>
+                        </div>
+                    </li>
+                    <li>
+                        <strong>Freelance Translator</strong>
+                        <div className="mt-1">
+                            <p className="text-sm text-gray-600 dark:text-gray-400">• Shanghai Education Publishing House (Apr 2023 - Present)</p>
                             <p className="text-sm text-gray-600 dark:text-gray-400">• Shanghai Conservatory of Music Press (Jan 2018 - Jun 2018)</p>
                         </div>
                     </li>
