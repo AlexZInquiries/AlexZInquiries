7bd21d7148eb08fc8620e4bac1b1627d119b8388
RealAlexZ <zhiyuzha@umich.edu>
2024-09-19 16:48:32 -0400

    Debugged for deployment

--- app/projects/[projectName]/page.tsx
+++ app/projects/[projectName]/page.tsx
@@ -30,11 +30,18 @@ export default async function ProjectPage({ params }: PageProps) {
         mediaUrls[projectName] = [];
     }
 
+    const amis2023Url = await getPhotoUrl("publications/amis-2023-presentation.pdf");
+    const amis2024Url = await getPhotoUrl("publications/amis-2024-poster.pdf");
+    const aimc2024Url = await getPhotoUrl("publications/aimc-2024-paper.pdf");
+
     // Render the ItemDetails component
     return (
         <ItemDetails
         itemKey={projectName}
         mediaUrls={mediaUrls}
+        amis2023Url={amis2023Url}
+        amis2024Url={amis2024Url}
+        aimc2024Url={aimc2024Url}
         />
     );
 }
