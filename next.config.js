6c7d0afcb43f5adc548a9587fd7c25f5eab53c8c
RealAlexZ <zhiyuzha@umich.edu>
2024-09-20 12:53:29 -0400

    Debugged for deployment

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
 	output: "export", // Add this line for static site generation
 	...(isProd && {
-		// basePath: "/personal-website-clone",
-		// assetPrefix: "/personal-website-clone/",
 	}),
 	images: {
 		domains: ["firebasestorage.googleapis.com", "raw.githubusercontent.com"],
